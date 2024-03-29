e074b80c3048458b7b94f716d6c8de1b5e6539bb
R3furbished <migas.2.3@outlook.pt>
2024-03-29 22:34:22 +0000

    Added loop for input retrieval and break condition

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    //main needs a loop to check user input against possible commands
+    //TODO add possible commands.
+
+    loop {
+        let mut raw_in = String::new();
+        io::stdin()
+            .read_line(&mut raw_in)
+            .expect("Failed to get input from usr.");
+
+        match raw_in.trim() {
+            "quit" => break,
+            _ => println!("Continue"),
+        }
+    }
 }
